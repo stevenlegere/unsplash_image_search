43cc1adcb09363d5d58b5ed02bb2ca1671ad25b9
stevenlegere <stevenmarklegere@gmail.com>
2023-10-10 20:51:16 +0100

    Fixed load error

--- unsplash_image_search/src/App.jsx
+++ unsplash_image_search/src/App.jsx
@@ -12,31 +12,37 @@ const App = () => {
   const [images, setImages] = useState([]);
   const [totalPages, setTotalPages] = useState(0);
   const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState('');
 
       // Add a new fetchImages function inside the App component
       const fetchImages = useCallback(async () => {
         try {
           if (searchInput.current.value) {
-          const { data } = await axios.get(
-            `${API_URL}?query=${
-              searchInput.current.value
-            }&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${
-              import.meta.env.VITE_API_KEY
-            }`
-          );
-          console.log('data', data);
-          setImages(data.results);
-          setTotalPages(data.total_pages);
+            setErrorMsg('');
+            setLoading(true);
+            const { data } = await axios.get(
+              `${API_URL}?query=${
+                searchInput.current.value
+              }&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${
+                import.meta.env.VITE_API_KEY
+              }`
+            );
+            setImages(data.results);
+            setTotalPages(data.total_pages);
+            setLoading(false);
           }
-            } catch (error) {
-              console.log(error);
-            }
-        }, [page]);
+        } catch (error) {
+          setErrorMsg('Error fetching images. Try again later.');
+          console.log(error);
+          setLoading(false);
+        }
+      }, [page]);
 
         
   useEffect(() => {
     fetchImages();
-  }, [fetchImages, page]);
+  }, [fetchImages]);
 
 
   const resetSearch = () => {
@@ -78,28 +84,31 @@ const App = () => {
         <div onClick={() => handleSelection('cats')}>Cats</div>
         <div onClick={() => handleSelection('shoes')}>Shoes</div>
       </div>
-
-      <div className='images'>
+      {loading ? (
+  <p className='loading'>Loading...</p>
+) : (
+  <>
+    <div className='images'>
       {images.map((image) => (
-          <img
-            key={image.id}
-            src={image.urls.small}
-            alt={image.alt_description}
-            className='image'
-          />
-        
+        <img
+          key={image.id}
+          src={image.urls.small}
+          alt={image.alt_description}
+          className='image'
+        />
       ))}
     </div>
     <div className='buttons'>
-  {page > 1 && (
-    <Button onClick={() => setPage(page - 1)}>Previous</Button>
-  )}
-  {page < totalPages && (
-    <Button onClick={() => setPage(page + 1)}>Next</Button>
-  )}
-</div>
+      {page > 1 && (
+        <Button onClick={() => setPage(page - 1)}>Previous</Button>
+      )}
+      {page < totalPages && (
+        <Button onClick={() => setPage(page + 1)}>Next</Button>
+      )}
     </div>
-  )
-};
-
+  </>
+)}
+    </div>
+  );
+}
 export default App;
